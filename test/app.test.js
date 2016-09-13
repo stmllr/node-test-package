9fbdeacb32363dab6819ad93872b57e5b1ce9eaa
Steffen Müller <typo3@t3node.com>
2016-09-13 11:18:09 +0200

    Fix cgl

--- test/app.test.js
+++ test/app.test.js
@@ -8,18 +8,18 @@ var webSocketServer = require('../app.js');
 var url = 'ws://' + process.env.HOST + ':' + process.env.PORT;
 
 describe('Websocket Server', function() {
-  it('should echo a message', function(done) {
-    var ws = new webSocket(url);
-    ws.on('open', function () {
-      var data = 'hello world';
-      ws.send(data);
-      ws.on('message', function(message) {
-        assert.equal(data, message);
-        done();
-        ws.close();
-      });
+    it('should echo a message', function(done) {
+        var ws = new webSocket(url);
+        ws.on('open', function () {
+            var data = 'hello world';
+            ws.send(data);
+            ws.on('message', function(message) {
+                assert.equal(data, message);
+                done();
+                ws.close();
+            });
+        });
     });
-  });
 });
 
 function randomPort() {
