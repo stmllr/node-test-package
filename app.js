66b29f8189d55ce52e09472fa08e4cb70526336e
Steffen Müller <typo3@t3node.com>
2016-09-13 11:21:36 +0200

    Send an initial 'hello' message to trigger the message event twice

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var host = process.env.HOST || '127.0.0.1';
 var myServer = new WebSocketServer({ port: port, host: host });
 
 myServer.on('connection', function (socket) {
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send('hello');
+    }
     socket.on('message', function (message) {
         socket.send(message);
     });
--- test/app.test.js
+++ test/app.test.js
@@ -11,9 +11,16 @@ describe('Websocket Server', function() {
     it('should echo a message', function(done) {
         var ws = new webSocket(url);
         ws.on('open', function () {
-            var data = 'hello world';
+            var data = 'This is a message';
             ws.send(data);
             ws.on('message', function(message) {
+                /** @FIXME message event is triggered twice. We should ignore 'hello'.
+                 * 
+                 * The following line is a hack, but comes with a test smell:
+                 * If the assertion is skipped if message does not match data,
+                 * then the assertion cannot fail by definition.
+                 */
+                // if (message !== data) return;
                 assert.equal(data, message);
                 done();
                 ws.close();
