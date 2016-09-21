a57e0213c02eb9d78430b165a1faed2835f5400b
Steffen Müller <typo3@t3node.com>
2016-09-21 21:17:03 +0200

    Simplify app

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var WebSocket = require('ws');
-var WebSocketServer = WebSocket.Server;
 
 var port = process.env.PORT || 3001;
 var host = process.env.HOST || '127.0.0.1';
 
-var myServer = new WebSocketServer({ port: port, host: host });
+var myServer = new WebSocket.Server({ port: port, host: host });
 
 myServer.on('connection', function (socket) {
     socket.on('message', function (message) {
